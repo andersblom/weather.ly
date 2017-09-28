3580abae3b79ff00bcbd3713878866356e1aefe1
Anders Blom <anders@andersblom.dk>
2017-09-27 21:21:49 -0400

    styling for settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -51,17 +51,63 @@ export default class Settings extends Component {
     return (
       <div>
         <Header {...this.props} showBackBtn={true} showSettings={false} />
-        <div>Settings</div>
-        <div>
-          <button style={styles.changeSettingButton} onClick={(e) => {this.handleTemperatureChange(e)}} name="celsius">Celsius</button>
-          <button style={styles.changeSettingButton} onClick={(e) => {this.handleTemperatureChange(e)}} name="fahrenheit">Fahrenheit</button>
-        </div>
-        <div>
-          <button style={styles.changeSettingButton} onClick={(e) => {this.handleDistanceChange(e)}} name="meters">Meters</button>
-          <button style={styles.changeSettingButton} onClick={(e) => {this.handleDistanceChange(e)}} name="feet">feet</button>
-        </div>
-        <div>
-          <button style={styles.changeSettingButton} onClick={() => { this.saveAndClose() }}>Save</button>
+        <div style={styles.settingsContainer}>
+          <div style={styles.settingsHeader}>Settings</div>
+
+          <div style={styles.unitHeader}>Temperature Unit</div>
+          <div>
+            <button 
+              style={(this.state.temperatureSetting === "celsius" ? 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonLeft, ...styles.changeSettingButtonHover} 
+                : 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonLeft}
+              )}  
+              onClick={(e) => {this.handleTemperatureChange(e)}} 
+              name="celsius">
+              Celsius
+            </button>
+            <button 
+              style={(this.state.temperatureSetting === "fahrenheit" ? 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonRight, ...styles.changeSettingButtonHover} 
+                : 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonRight}
+              )}
+              onClick={(e) => {this.handleTemperatureChange(e)}} 
+              name="fahrenheit">
+              Fahrenheit
+            </button>
+          </div>
+
+          <div style={styles.unitHeader}>Distance Unit</div>
+          <div>
+            <button 
+              style={(this.state.distanceSetting === "meters" ? 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonLeft, ...styles.changeSettingButtonHover} 
+                : 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonLeft}
+              )}
+              onClick={(e) => {this.handleDistanceChange(e)}} 
+              name="meters">
+              Meters
+            </button>
+            <button 
+              style={(this.state.distanceSetting === "feet" ? 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonRight, ...styles.changeSettingButtonHover} 
+                : 
+                {...styles.changeSettingButton, ...styles.changeSettingButtonRight}
+              )}
+              onClick={(e) => {this.handleDistanceChange(e)}} 
+              name="feet">
+              Feet
+            </button>
+          </div>
+          <div style={styles.submitButtonContainer}>
+            <button 
+              style={styles.submitButton} 
+              onClick={() => { this.saveAndClose() }}>
+              Save
+            </button>
+          </div>
         </div>
       </div>
     );
@@ -69,16 +115,79 @@ export default class Settings extends Component {
 }
 
 const styles = {
+  settingsContainer: {
+    height: "calc(100vh - 230px)", // 115 (Header: 90px height + 25px margin) * 2 to account for top/bottom
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItem: "center",
+    textAlign: "center",
+  },
+  
+  settingsHeader: {
+    fontFamily: sharedStyles.font,
+    fontSize: "20px",
+    color: sharedStyles.color.brightGray,
+    marginBottom: "40px",
+  },
+
+  unitHeader: {
+    marginBottom: "20px",
+    fontFamily: sharedStyles.font,
+    fontSize: "14px",
+    textTransform: "uppercase",
+    letterSpacing: "1px",
+    color: sharedStyles.color.fadedBlue,
+  },
+
   changeSettingButton: {
-    marginTop: "40px",
+    marginBottom: "40px",
+    backgroundColor: "transparent",
     padding: "1.2em 3.4em 1.2em 3.4em",
-    border: "3px solid " + sharedStyles.color.blue,
-    color: sharedStyles.color.blue,
+    border: "3px solid",
+    borderColor: sharedStyles.color.fadedBlue,
+    color: sharedStyles.color.fadedBlue,
     fontFamily: sharedStyles.font,
     textDecoration: "none",
     textTransform: "uppercase",
     fontSize: "14px",
     fontWeight: "bold",
+    position: "relative",
+    zIndex: "0",
+  },
+  changeSettingButtonLeft: {
+    borderRadius: "100px 0px 0px 100px",
+  },
+  changeSettingButtonRight: {
+    marginLeft: "-3px",
+    borderRadius: "0px 100px 100px 0px",
+  },
+  changeSettingButtonHover: {
+    borderColor: sharedStyles.color.fadedBlue,
+    color: sharedStyles.color.white,
+    backgroundColor: sharedStyles.color.fadedBlue,
+    outline: "none",
+    zIndex: "1",
+  },
+
+  submitButtonContainer: {
+    marginTop: "auto",
+  },
+
+  submitButton: {
+    backgroundColor: "transparent",
+    padding: "1.2em 3.4em 1.2em 3.4em",
+    border: "3px solid",
+    fontFamily: sharedStyles.font,
+    textDecoration: "none",
+    textTransform: "uppercase",
+    fontSize: "14px",
+    fontWeight: "bold",
+    position: "relative",
+    zIndex: "0",
+
+    borderColor: sharedStyles.color.blue,
+    color: sharedStyles.color.blue,
     borderRadius: "100px",
   }
 }
--- src/resources/sharedStyles.js
+++ src/resources/sharedStyles.js
@@ -3,6 +3,7 @@ const sharedStyles = {
         blue: "#2C69EE",
         fadedBlue: "#AFBCD3",
         brightGray: "#B4BBC7",
+        white: "#FFFFFF",
     },
     font: "system, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Lucida Grande'",
 }
