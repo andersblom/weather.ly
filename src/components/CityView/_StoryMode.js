fdbfa7a332e4bfba1f41793b25f807642794198e
Anders Blom <anders@andersblom.dk>
2017-09-30 22:34:44 -0400

    Some sentencebuilding

--- src/components/CityView/_StoryMode.js
+++ src/components/CityView/_StoryMode.js
@@ -5,6 +5,62 @@ import moment from 'moment';
 import sharedStyles from '../../resources/sharedStyles';
 
 export default class StoryMode extends Component {
+  introMessage() {
+    let weather = this.props.singleCityData.data.weather[0].icon;
+
+    switch(weather) {
+      /* eslint-disable */
+      // Clear sky
+      case "01d":
+      case "01n":
+      // Few clouds
+      case "02d":
+      case "02n":
+        return "It's a beeaaautiful day";
+        break;
+
+      // Scattered clouds
+      case "03d":
+      case "03n":
+      // Broken Clouds
+      case "04d":
+      case "04n":
+        return "It's a somewhat cloudy day";
+        break;
+
+      // Shower rain
+      case "09d":
+      case "09n":
+      // Rain
+      case "10d":
+      case "10n":
+        return "Uh oh. Today is a rainy one";
+        break;
+
+      // Thunderstorm
+      case "11d":
+      case "11n":
+        return "Better not be playing golf, because you're getting thunderstorms";
+        break;
+
+      // Snow
+      case "13d":
+      case "13n":
+        return "It's snowing!";
+        break;
+
+      // Mist
+      case "50d":
+      case "50n":
+        return "It's kind of foggy/misty";
+        break;
+
+      // No match (Could happen because of API problems/changes)
+      default: 
+        return "It's gonna be a good day!";
+      /* eslint-enable */
+    }
+  }
   getCityName() {
     return this.props.singleCityData.userInput;
   }
@@ -17,36 +73,36 @@ export default class StoryMode extends Component {
     const getWindDirection = (windDeg) => {
       /* eslint-disable */
       switch(true) {
-        case windDeg => 0 && windDeg <= 10:
-        case windDeg > 165 && windDeg <= 180:
+        case windDeg => 0 && windDeg <= 20:
+        case windDeg > 340 && windDeg <= 360:
           return "North";
           break;
 
-        case windDeg > 10 && windDeg <= 35:
+        case windDeg > 20 && windDeg <= 60:
           return "North East";
           break;
 
-        case windDeg > 35 && windDeg <= 55:
+        case windDeg > 60 && windDeg <= 120:
           return "East";
           break;
 
-        case windDeg > 55 && windDeg <= 80:
+        case windDeg > 120 && windDeg <= 160:
           return "South East";
           break;
 
-        case windDeg > 80 && windDeg <= 100:
+        case windDeg > 160 && windDeg <= 220:
           return "South";
           break;
 
-        case windDeg > 100 && windDeg <= 125:
+        case windDeg > 220 && windDeg <= 260:
           return "South West";
           break;
 
-        case windDeg < 125 && windDeg <= 145: 
+        case windDeg < 260 && windDeg <= 320: 
           return "West";
           break;
 
-        case windDeg < 145 && windDeg <= 165:
+        case windDeg < 320 && windDeg <= 340:
           return "North West"
           break;
 
@@ -79,17 +135,51 @@ export default class StoryMode extends Component {
     return date;
   }
   
+  measureHumidity() {
+    let humidity = this.props.singleCityData.data.main.humidity;
+    
+    switch(true) {
+      case humidity <= 40:
+        return "It's very dry"; 
+
+      case humidity > 40 && humidity < 65:
+        return `It's not too humid`; 
+
+      case humidity >= 65 && humidity < 90:
+        return "It's a little humid";
+
+      case humidity >= 90:
+        return "It's very humid";
+
+      default: 
+        return "Humidity is " + humidity + "%";
+    }
+  }
+
+  showCloudPercentage() {
+    let cloudPercentage = this.props.singleCityData.data.clouds.all;
+    switch(true) {
+      case cloudPercentage <= 15:
+        return "there will be no problem seing the stars tonight, with this clear sky!"
+      
+      case cloudPercentage > 15 && cloudPercentage <= 35:
+        return `the sky is somewhat clear, with only ${cloudPercentage}% skies!` 
+      
+      case cloudPercentage > 35 && cloudPercentage <= 60:
+        return `it's kind of cloudy, around ${cloudPercentage}% will be covered in skies.` 
+      
+      case cloudPercentage > 60:
+        return `It's very cloudy, so you won't be needing your sunglasses today!` 
+    }
+    return "there is plenty of chance to see the stars with only 21% clouds.";
+  }
+
   render() {
     console.log(this.props.singleCityData);
     return (
       <div style={styles.storyModeContainer}>
-        It’s a beautiful night in <span style={styles.highlight}>{this.getCityName()}</span>, 
-        with a clear blue sky, {this.getTemperatureInUnit()}
-        {/* TODO: fix direction because math 🤷‍♀️  */}
-        and {this.getWindSpeedAndDirection()}. 
-        The sunrise is at {this.getTime(this.props.singleCityData.data.sys.sunrise)} and sunset at {this.getTime(this.props.singleCityData.data.sys.sunset)}. 
-        It’s not too humid, and there is plenty of 
-        chance to see the stars with only 21% clouds.
+        {this.introMessage()} in <span style={styles.highlight}>{this.getCityName()}</span>, with a clear blue sky, <span style={styles.highlight}>{this.getTemperatureInUnit()}</span> and <span style={styles.highlight}>{this.getWindSpeedAndDirection()}</span>. 
+        The sunrise is at <span style={styles.highlight}>{this.getTime(this.props.singleCityData.data.sys.sunrise)}</span> and sunset at <span style={styles.highlight}>{this.getTime(this.props.singleCityData.data.sys.sunset)}</span>. {this.measureHumidity()}, and {this.showCloudPercentage()}
       </div>
     );
   }
