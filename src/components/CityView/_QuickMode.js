d46bbe5bcc2a2441f4facef4cb6c7ae75f7f1bba
Anders Blom <anders@andersblom.dk>
2017-10-08 21:52:04 -0400

    Did temperature calculation in QuickMode

--- src/components/CityView/_QuickMode.js
+++ src/components/CityView/_QuickMode.js
@@ -6,6 +6,30 @@ import CloudIcon from '../../resources/icons/cloud.svg';
 import WindIcon from '../../resources/icons/wind.svg';
 
 export default class QuickMode extends Component {
+  getTemperature() {
+    let kelvin = this.props.singleCityData.data.main.temp;
+    let unit = this.props.appSettingUnitTemp;
+    if (unit === "celsius") {
+      let calculatedNumber = kelvin - 273.15;
+      calculatedNumber = calculatedNumber.toFixed(0);
+      return calculatedNumber;
+    } else {
+      let calculatedNumber = kelvin * 9/5 - 459.67;
+      calculatedNumber = calculatedNumber.toFixed(0);
+      return calculatedNumber;
+    }
+  },
+
+  getTemperatureUnit() {
+    let unit = this.props.appSettingUnitTemp;
+
+    if (unit === "celsius") {
+      return "°C";
+    } else {
+      return "°F";
+    }
+  },
+  
   render() {
     console.log(this.props);
     return (
@@ -14,7 +38,7 @@ export default class QuickMode extends Component {
           <div style={styles.weatherResPreCity}>Todays weather for</div>
           <div style={styles.weatherResCityName}>{this.props.singleCityData.data.name}</div>
         </div>
-        <div style={styles.temperature}>81 <span style={styles.tempUnit}>°F</span></div>
+        <div style={styles.temperature}>{this.getTemperature()} <span style={styles.tempUnit}>{this.getTemperatureUnit()}</span></div>
         <div style={styles.weatherExpContainer}>
           <div style={styles.weatherExpIcon}><img src={WindIcon} alt="Todays weather" /></div>
           <div>
