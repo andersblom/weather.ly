acf05a8fc902bd983f567d65dccf08b5c7ca81fa
Anders Blom <anders@andersblom.dk>
2017-10-09 20:17:25 -0400

    init readme + fixed NotFound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import Header from './Header';
+
+import sharedStyles from '../resources/sharedStyles';
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div>
-        404 xd
+      <div style={styles.container}>
+        <Header {...this.props} showBackBtn={true} showSettings={false} />
+        <div style={styles.textContainer}>
+          <div>Whoops. Not found! :(</div>
+        </div>
       </div>
     );
+  }
+}
+
+const styles = {
+  container: {
+    fontFamily: sharedStyles.font,
+  },
+
+  textContainer: {
+    ...sharedStyles.extends.vh.minusHeaderAndBottom,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   }
 }
